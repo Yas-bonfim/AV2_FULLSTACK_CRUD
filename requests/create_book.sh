3803df2e6c840a2ac40b879ced599072e384e1d9
Yasmin Bonfim <yasminbwork@gmail.com>
2025-05-08 16:56:25 -0300

    atualizando os docs

--- requests/create_book.sh
+++ requests/create_book.sh
@@ -1,4 +1,4 @@
 curl -X POST http://localhost:3000/api/books \
-  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MWJkNTAyM2MxMzVmOWI3NjhjZWVlMSIsImVtYWlsIjoiYW5hQGVtYWlsLmNvbSIsImlhdCI6MTc0NjcyMzQyMiwiZXhwIjoxNzQ2NzI3MDIyfQ.avdkxfOIoVKU5t3GXDu0D0H0KNmGVUPxyJUkhuqMmOI" \
+  -H "Authorization: Bearer <token>" \
   -H "Content-Type: application/json" \
   -d '{"title":"Clean Code", "author":"Robert C. Martin", "year":2008, "genre":"Software Engineering"}'
--- requests/update_totally_book.sh
+++ requests/update_totally_book.sh
@@ -1,4 +1,4 @@
-curl -X PUT http://localhost:3000/api/books/681ce765ca0fab92bc0ca0c1 \
-  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MWJkNTAyM2MxMzVmOWI3NjhjZWVlMSIsImVtYWlsIjoiYW5hQGVtYWlsLmNvbSIsImlhdCI6MTc0NjcyMzQyMiwiZXhwIjoxNzQ2NzI3MDIyfQ.avdkxfOIoVKU5t3GXDu0D0H0KNmGVUPxyJUkhuqMmOI" \
+curl -X PUT http://localhost:3000/api/books/bookid \
+  -H "Authorization: Bearer <token>" \
   -H "Content-Type: application/json" \
   -d '{"title":"Clean Architecture","author":"Robert C. Martin","year":2017,"genre":"Software Architecture"}'
